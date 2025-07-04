4d40d6711ae38d67ce32401a2f81019643a5569a
satya-svt <satyavardhanthinnaluri@gmail.com>
2025-07-05 03:52:23 +0530

    updated for deploying

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,20 @@
-import { StrictMode } from 'react';
-import { createRoot } from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
-import App from './App.tsx';
-import './index.css';
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import { BrowserRouter } from 'react-router-dom'
+import App from './App.tsx'
+import './index.css'
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </StrictMode>
-);
+// Make sure the root element exists in your index.html
+const rootElement = document.getElementById('root')
+
+if (rootElement) {
+  createRoot(rootElement).render(
+    <StrictMode>
+      <BrowserRouter basename="/">
+        <App />
+      </BrowserRouter>
+    </StrictMode>
+  )
+} else {
+  console.error('Root element not found.')
+}
